68bae3c6539e1ae6cf32c03024f56483f2206dc7
andy <a.chaa@marketinvoice.com>
2016-02-16 14:09:23 +0000

    Fix for the issue that LogManager.GetCurrentClassLogger returns only CorrelatorSharp

--- CorrelatorSharp.Logging/LogManager.cs
+++ CorrelatorSharp.Logging/LogManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace CorrelatorSharp.Logging
 {
@@ -6,12 +8,38 @@ namespace CorrelatorSharp.Logging
     {
         public static ILogger GetCurrentClassLogger()
         {
-            return LoggingConfiguration.LogManager.GetCurrentClassLogger();
+            var name = GetClassFullName();
+            return GetLogger(name);
         }
 
         public static ILogger GetLogger(string name)
         {
             return LoggingConfiguration.LogManager.GetLogger(name);
+        }
+
+        // From NLog's LogManager code
+        private static string GetClassFullName()
+        {
+            string className;
+            Type declaringType;
+            var framesToSkip = 2;
+
+            do
+            {
+                var frame = new StackFrame(framesToSkip, false);
+                var method = frame.GetMethod();
+                declaringType = method.DeclaringType;
+                if (declaringType == null)
+                {
+                    className = method.Name;
+                    break;
+                }
+
+                framesToSkip++;
+                className = declaringType.FullName;
+            } while (declaringType.Module.Name.Equals("mscorlib.dll", StringComparison.OrdinalIgnoreCase));
+
+            return className;
         }
 
     }
